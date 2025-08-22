0940784329466a4ae05e3d3da49986f6ab09fe56
Minhoi Goo <inojyes@gmail.com>
2025-08-22 11:40:03 +0900

    [추가] docs 폴더 추가

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "BoxOfficesKit",
-  platforms: [.iOS(.v15)],
+  platforms: [.iOS(.v15), .macOS(.v13)],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
@@ -14,7 +14,7 @@ let package = Package(
 //    .executable(name: <#T##String#>, targets: <#T##[String]#>)
   ],
   dependencies: [
-//    .package(id: <#T##String#>, from: <#T##Version#>)
+    .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main")
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
